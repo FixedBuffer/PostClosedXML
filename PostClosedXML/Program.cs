a03528fb266b640f62e588f8070e2ac6e6f0f1c4
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-09-10 22:44:29 +0200

    Final

--- PostClosedXML/Program.cs
+++ PostClosedXML/Program.cs
@@ -1,4 +1,6 @@
 ﻿using ClosedXML.Excel;
+using System.IO;
+using System.Linq;
 
 namespace PostClosedXML
 {
@@ -6,12 +8,38 @@ namespace PostClosedXML
   {
     static void Main(string[] args)
     {
+      //Si no existe el fichero xlsx, pasamos a la primera parte, y sino a la segunda
+      if (!File.Exists("HelloWorld.xlsx"))
+        PrimeraParte();
+      else
+        SegundaParte();
+
+    }
+
+    static void PrimeraParte()
+    {
       using (var workbook = new XLWorkbook())
       {
         var worksheet = workbook.Worksheets.Add("Sample Sheet");
         worksheet.Cell("A1").Value = "Hello World!";
         worksheet.Cell("A2").FormulaA1 = "=MID(A1, 7, 5)";
         workbook.SaveAs("HelloWorld.xlsx");
+      }
+    }
+
+    static void SegundaParte()
+    {
+      using (var workbook = new XLWorkbook("HelloWorld.xlsx"))
+      {
+        //Buscamos con LinQ la hohja que nos interesa copiar
+        var SampleSheet = workbook.Worksheets.Where(x => x.Name == "Sample Sheet").First();
+        //Añadimos una hoja nuevo
+        var worksheet = workbook.Worksheets.Add("FixedBuffer");
+        //Copiamos los valores
+        worksheet.Cell("A1").Value = SampleSheet.Cell("A1").GetString().ToUpper();
+        worksheet.Cell("A2").FormulaA1 = SampleSheet.Cell("A2").FormulaA1;
+        //Guardamos el libro
+        workbook.Save();
       }
     }
   }
